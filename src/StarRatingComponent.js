27ef4f0d5667fce1563425904d340d7a64f675af
Gustavo Cavalcante <nowayhecodes@gmail.com>
2020-04-26 03:47:46 -0300

    added star component

---
+++ src/StarRatingComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+import "./star.css";
+
+const Star = ({ selected = false, onClick = (f) => f }) => (
+  <div className={selected ? "star selected" : "star"} onClick={onClick}></div>
+);
+
+Star.propTypes = {
+  selected: PropTypes.bool,
+  onClick: PropTypes.func,
+};
